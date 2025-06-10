36f90ae0268def5746625793c7d7b31b9e1b5a39
David Rios <david.rios.gomes@gmail.com>
2025-06-10 07:58:06 -0300

    Adjust lua line

--- lua/plugins.available/lualine.lua
+++ lua/plugins.available/lualine.lua
@@ -2,13 +2,13 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
+    config = function()
       require('lualine').setup {
         options = {
           icons_enabled = false,
           theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
           disabled_filetypes = {
             statusline = {},
             winbar = {},
@@ -24,17 +24,17 @@ return {
           }
         },
         sections = {
-          lualine_a = {'mode'},
+          lualine_a = { 'mode' },
           lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
+          lualine_c = { 'filename' },
+          lualine_x = { 'lsp_status', 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
         },
         inactive_sections = {
           lualine_a = {},
-          lualine_b = {'filename'},
-          lualine_x = {'location'},
+          lualine_b = {},
+          lualine_x = { 'location' },
           lualine_y = {},
           lualine_z = {}
         },
