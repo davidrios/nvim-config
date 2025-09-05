2bd51a8e066fbb421a5a3000ef918abfcd8a54cb
David Rios <david.rios.gomes@gmail.com>
2025-09-04 21:39:27 -0300

    Add vue injection

---
+++ queries/ecma/injections.scm
@@ -0,0 +1,11 @@
+(export_statement
+  value: (object
+		   (pair
+			 key: (property_identifier) @key (#eq? @key "template")
+			 value: (template_string
+					  (string_fragment) @injection.content)
+			 (#set! injection.language "vue")
+			 (#set! injection.combined)
+			 )
+		   ))
+
