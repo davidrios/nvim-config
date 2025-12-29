ea4438312a51cc6e72ccf64c95ff50421942a3c1
David Rios <david.rios.gomes@gmail.com>
2025-12-29 12:23:49 -0300

    Add a couple of custom injections

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -8,3 +8,18 @@
 	)
   )
 
+(variable_declarator
+  name: (identifier) @name (#eq? @name "CSS_STYLE")
+  value: (template_string
+		   (string_fragment) @injection.content)
+  (#set! injection.language "css")
+  (#set! injection.combined)
+  )
+
+(variable_declarator
+  name: (identifier) @name (#eq? @name "HTML_TEMPLATE")
+  value: (template_string
+		   (string_fragment) @injection.content)
+  (#set! injection.language "html")
+  (#set! injection.combined)
+  )
